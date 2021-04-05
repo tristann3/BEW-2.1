fc9a7b518d3a7969593dbe36509031cfec879105
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-05 16:30:37 -0700

    init commit
    cloned starter and added comments

---
+++ petes-pets/server.js
@@ -0,0 +1,61 @@
+if (!process.env.PORT) {
+  require('dotenv').config()
+  process.env.NODE_ENV = "dev"
+}
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/petes-pets');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/*
+  override with POST having ?_method=DELETE or ?_method=PUT
+  this allows us to create POST requests but override them with put or delete
+*/
+app.use(methodOverride('_method'))
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// logger logs HTTP requests in the server terminal with 'dev' formatting
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//allows us to get signed and unsigned cookies from our routes using req.~
+app.use(cookieParser());
+
+
+require('./routes/index.js')(app);
+require('./routes/pets.js')(app);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
