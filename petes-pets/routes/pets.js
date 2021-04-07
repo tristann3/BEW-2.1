e3e07aaf2e86da568bc7ea6834deb574ba9edc7f
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-06 21:26:43 -0700

    [ADD] Implemented Search Pagination

--- petes-pets/routes/pets.js
+++ petes-pets/routes/pets.js
@@ -8,16 +8,21 @@ module.exports = (app) => {
 
   // SEARCH PET
   app.get('/search', (req, res) => {
-    //The search term entered in the form
-    term = new RegExp(req.query.term, 'i')
 
-    Pet.find({$or:[
-    {'name': term},
-    {'species': term}
-  ]}).exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
-  })
-});
+    const term = new RegExp(req.query.term, 'i')
+
+    const page = req.query.page || 1
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
+      });
+  });
 
   // NEW PET
   app.get('/pets/new', (req, res) => {
