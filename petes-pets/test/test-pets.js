5706f2c181fc60d2f4eb3b3dd790eb4cb77f8dd4
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-14 12:07:26 -0700

    Implemented resopnding to JSON

--- petes-pets/test/test-pets.js
+++ petes-pets/test/test-pets.js
@@ -128,4 +128,16 @@ describe('Pets', ()  => {
       done();
     });
   });
+
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+    .get('/')
+    .set('content-type', 'application/json')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      done();
+    });
+  });
 });
