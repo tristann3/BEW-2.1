3dbf3486586ca6db6835df9e8817dfa4f0112a3b
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-26 16:55:27 -0700

    Users can log in

--- Make-Chat/app.js
+++ Make-Chat/app.js
@@ -6,7 +6,8 @@ const server = require("http").Server(app);
 //Socket.io
 const io = require("socket.io")(server);
 io.on("connection", socket => {
-  console.log("🔌 New user connected! 🔌");
+  // This file will be read on new socket connections
+  require("./public/sockets/chat.js")(io, socket);
 });
 
 const exphbs = require("express-handlebars");
