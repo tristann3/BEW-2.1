39b2a94a1e284883c040ada74190cb0a93816346
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-26 18:17:02 -0700

    [Hotfix] missing parens

--- Make-Chat/public/sockets/chat.js
+++ Make-Chat/public/sockets/chat.js
@@ -40,10 +40,14 @@ module.exports = (io, socket, onlineUsers, channels) => {
   });
 
   //Have the socket join the room of the channel
-  socket.on('user changed channel', (newChannel) => {
-    socket.join(newChannel);
-    socket.emit('user changed channel', {
-      channel : newChannel,
-      messages : channels[newChannel]
+socket.on('user changed channel', (newChannel) => {
+  socket.join(newChannel);
+  socket.emit('user changed channel', {
+    channel : newChannel,
+    messages : channels[newChannel]
   });
+  //???
+})
+
+
 };
